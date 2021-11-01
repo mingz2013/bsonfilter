b01ceac86ff28690a94ae1bd9ee4180838d2dc80
mingz <305603665@qq.com>
2021-11-01 18:48:07 +0800

    ast parser

--- interpreter/rawwrapper/compare.go
+++ interpreter/rawwrapper/compare.go
@@ -1,15 +1,77 @@
 package rawwrapper
 
-import "go.mongodb.org/mongo-driver/bson"
+import (
+	"github.com/mongodb/mongo-tools/common/log"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 const eq = 0
 const greater = 1
 const less = -1
 
+//const (
+//	TypeDouble           = bsontype.Double
+//	TypeString           = bsontype.String
+//	TypeEmbeddedDocument = bsontype.EmbeddedDocument
+//	TypeArray            = bsontype.Array
+//	TypeBinary           = bsontype.Binary
+//	TypeUndefined        = bsontype.Undefined
+//	TypeObjectID         = bsontype.ObjectID
+//	TypeBoolean          = bsontype.Boolean
+//	TypeDateTime         = bsontype.DateTime
+//	TypeNull             = bsontype.Null
+//	TypeRegex            = bsontype.Regex
+//	TypeDBPointer        = bsontype.DBPointer
+//	TypeJavaScript       = bsontype.JavaScript
+//	TypeSymbol           = bsontype.Symbol
+//	TypeCodeWithScope    = bsontype.CodeWithScope
+//	TypeInt32            = bsontype.Int32
+//	TypeTimestamp        = bsontype.Timestamp
+//	TypeInt64            = bsontype.Int64
+//	TypeDecimal128       = bsontype.Decimal128
+//	TypeMinKey           = bsontype.MinKey
+//	TypeMaxKey           = bsontype.MaxKey
+//)
+
+func checkTypeSupport(value bson.RawValue) bool {
+	switch value.Type {
+	case bson.TypeInt32, bson.TypeInt64, bson.TypeDecimal128, bson.TypeDouble:
+		return true
+	default:
+		log.Logvf(log.Always, "type support error %v", value.Type)
+		return false
+	}
+}
+
 func Compare(left, right bson.RawValue) int {
 	// 0 ==
 	// 1 >
 	// -1 <
+	log.Logvf(log.Always, "Compare: left %v, right %v", left, right)
+	//if left.IsNumber() && right.IsNumber(){
+	//	if left.Double() > right.Double(){
+	//		return greater
+	//	}else if left.Double() < right.Double(){
+	//		return less
+	//	}else {
+	//		return eq
+	//	}
+	//} else {
+	//return bytes.Compare(left.Value, right.Value)
+	//}
+
+	//if left.IsNumber() && right.IsNumber(){
+	//	left.
+	//}
+
+	if checkTypeSupport(left) && checkTypeSupport(right) {
+		return false
+	}
+
+	switch left.Type {
+	case bson.TypeDouble:
+
+	}
 
 	return eq
 
@@ -41,5 +103,17 @@ func IsGreaterOrEq(left, right bson.RawValue) bool {
 }
 
 func IsIn(left, right bson.RawValue) bool {
-	return true
+	//raw := right.Array()
+	raw := right.Array()
+	elems, err := raw.Values()
+	if err != nil {
+		log.Logvf(log.Always, "error %v", err)
+	}
+	for _, elem := range elems {
+		if Compare(left, elem) == eq {
+			return true
+		}
+	}
+	return false
+
 }
