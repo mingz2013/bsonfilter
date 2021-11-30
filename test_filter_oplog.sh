dbab27924bb8d561af15aeb3f971cc4930b2b3a9
mingz <305603665@qq.com>
2021-11-30 18:49:49 +0800

    shell

--- test_filter_oplog.sh
+++ test_filter_oplog.sh
@@ -1,1 +1,1 @@
-./bin/bsonfilter.darwin -i oplog.bson -o tmp.bson -v -q '{"$and": [{"ns": "data.data"}, {"$or": [{"op": "u", "o2._id": {"$in": [45921229, 5001059177]}},{"op": "i", "o._id": {"$in": [45921229, 5001059177]}},{"op": "d", "o._id": {"$in": [45921229, 5001059177]}}]}]}'
+./bin/bsonfilter.darwin -i oplog.enc.bson -o tmp.bson -v -q '{"$and": [{"ns": {"$regex": "data.user_"}}, {"$or": [{"op": "u", "o2._id": {"$in": [45921229, 5001059177]}},{"op": "i", "o._id": {"$in": [45921229, 5001059177]}},{"op": "d", "o._id": {"$in": [45921229, 5001059177]}}]}]}'
